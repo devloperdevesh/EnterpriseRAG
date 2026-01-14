c1048edf12a03bc2bbda0d2b070169111319d5a6
Devesh Chauhan <deveshchauhandk_cse23@its.edu.in>
2026-01-14 23:09:24 +0530

    Add Vercel domain to CORS

--- app/main.py
+++ app/main.py
@@ -20,6 +20,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",   # Vite frontend
         "http://127.0.0.1:5173"
+        "https://enterpriserag-ai.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
