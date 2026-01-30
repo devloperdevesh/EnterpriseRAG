29b3e5ec7c49e3f1d1049a19daab4a8050580172
Devesh Chauhan <deveshchauhandk_cse23@its.edu.in>
2026-01-30 09:55:25 +0530

    Fixed DB issue

--- app/db/init_db.py
+++ app/db/init_db.py
@@ -1,4 +1,6 @@
-from app.db.session import engine, Base
+
+from app.db.session import engine
+from app.db.base import Base
 from app.models.user import User
 from app.models.tenant import Tenant
 from app.models.documents import Document
